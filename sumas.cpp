8745f9e8db0f3ea1a4547c223cc62fadbbd7e250
tres-xxx <tres.iqus2005@gmail.com>
2018-01-17 18:46:58 -0500

    sumas.cpp

--- sumas.cpp
+++ sumas.cpp
@@ -1,23 +1,57 @@
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 void operation(int m){
-	cout << "Introduce the amont of numbers to operate" << endl;
+	cout << "Introduce the amount of numbers to operate" << endl;
 	int amount_numbers;
 	cin >> amount_numbers;
-	int numbers[amount_numbers];
+	long long numbers[amount_numbers]; //Be careful!!!
 	cout << "Introduce the amount of digits" << endl;
 	int amount_digits;
 	cin >> amount_digits;
 	cout << "If you want to stop playing introduce 0" << endl;
+    amount_digits = pow(10,amount_digits);
 	int exit = 1;
 	while(exit == 1){
-		srand (time(NULL));
-		for(int i=0; i<amount_numbers; i++){
-			numbers[i] = rand() % amount + 
+        int result = 0;
+        srand (time(NULL));
+		for(int i=0; i < amount_numbers; i++){
+			numbers[i] = rand() % amount_digits;
+            if(i < (amount_numbers-1)){          
+                cout << numbers[i];                
+                switch(m){
+                    case 1: cout << " + ";
+                            result = result + numbers[i];
+                            break;
+                    default: cout << "error";
+                }
+            }
+            else{
+                cout << numbers[i];
+                switch(m){
+                    case 1: result = result + numbers[i];break;
+                    default: cout << "error";              
+                }
+            }
 		}
+        
+        cout << endl;
+        cout << "Introduce your answer = ";
+        long long answer;
+        cin >> answer;
+        while(answer != 0 && answer != result){
+            cout << "Try again" << endl;
+            cout << "Introduce your answer = ";
+            cin >> answer;
+        }
+        if(answer == 0) exit = 0;
+        else{
+            cout << "Good answer" << endl;
+        }
 	}
 };
 
@@ -30,7 +64,9 @@ void print_menu(){
 
 void menu(int op){
 	switch(op){
+        //(1)add
 		case 1: operation(1);break;
+        case 0: cout << "I hope you enjoy it stupid Camilo!!!" << endl; break;
 		default: cout << "No valid option" << endl;
 	}
 }
@@ -41,6 +77,7 @@ int main(){
 	do{
 		print_menu();
 		cin >> option_menu;
+        menu(option_menu);
 	}
 	while(option_menu!=0);
 }
