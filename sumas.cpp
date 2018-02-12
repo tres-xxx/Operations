cc7d863cadb7d5d7ed779a2ec9b17e93832ebf4d
tres-xxx <tres.iqus2005@gmail.com>
2018-02-11 22:48:18 -0500

    Added multiplication

--- sumas.cpp
+++ sumas.cpp
@@ -19,7 +19,9 @@ void operation(int m){
 	
     int exit = 1;
 	while(exit == 1){
-        int result = 0;
+        int result;
+        if(m!=3) result = 0;
+        else result = 1;
         srand (time(NULL));
 		for(int i=0; i < amount_numbers; i++){
 			numbers[i] = rand() % amount_digits;
@@ -33,6 +35,9 @@ void operation(int m){
                             if(i==0) result = numbers[i] - result;
                             else result-=numbers[i];
                             break;
+                    case 3: cout << " * ";
+                            result = result * numbers[i];
+                            break;
                     default: cout << "error";
                 }
             }
@@ -41,6 +46,7 @@ void operation(int m){
                 switch(m){
                     case 1: result = result + numbers[i];break;
                     case 2: result-=numbers[i]; break;
+                    case 3: result = result * numbers[i];break;
                     default: cout << "error";
                 }
             }
@@ -60,6 +66,7 @@ void operation(int m){
                 cout << endl << "Do you want to end the game? (0=No, 1= Yes): ";
                 cin >> answer;
                 if(answer == 1) exit = 0;
+                else cout << "Good answer" << endl;
             }
             else exit = 0;
         }
@@ -74,14 +81,17 @@ void print_menu(){
 	cout << "---->Press the option you want" << endl;
 	cout << "1. Add" << endl;
     cout << "2. Substract" << endl;
+    cout << "3. Multiplication" << endl;
 	cout << "0. Exit" << endl;
+    cout << "Tap: ";
 };
 
-void menu(int op){
+void menu(int op){ //This menu has no sense!!!!
 	switch(op){
         //(1)add
 		case 1: operation(1);break;
         case 2: operation(2);break;
+        case 3: operation(3);break;
         case 0: cout << "I hope you enjoy it!!!" << endl; break;
 		default: cout << "No valid option" << endl;
 	}
